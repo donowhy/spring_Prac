32bb9f920903e35ac9bec646de6af2ddb98e76bb
donowhy <119389068+donowhy@users.noreply.github.com>
2023-03-19 19:49:13 +0900

    finish real basic

--- hello/hello/src/main/java/hello/hello/service/SpringConfig.java
+++ hello/hello/src/main/java/hello/hello/service/SpringConfig.java
@@ -1,10 +1,23 @@
 package hello.hello.service;
 
+import hello.hello.repository.JpaMemberRepository;
 import hello.hello.repository.MemberRepository;
 import hello.hello.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+@Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -13,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
